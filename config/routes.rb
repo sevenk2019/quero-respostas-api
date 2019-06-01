ca995c19bc42b78fa04db4681985034235a17a45
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-06-01 20:43:35 -0300

    Prefix feed routes with /feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :courses
-  resources :tags
-  resources :answers
-  resources :questions
-  resources :users
-  controller 'feed' do
-    get '/feed' => :index
-    get '/tag/:tag' => :by_tag
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :courses
+    resources :tags
+    resources :answers
+    resources :questions
+    resources :users
+    scope :feed do
+        controller :feed do
+            get '/' => :index
+            get '/tag/:tag' => :by_tag
+        end
+    end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
