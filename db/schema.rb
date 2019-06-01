e8cb7bd7a5d7e308b6b58dfb5a493daab881b785
robisnei <robisneey@hotmail.com>
2019-06-01 17:46:57 -0300

    ignore schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_194727) do
+ActiveRecord::Schema.define(version: 2019_06_01_203702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "tag_id"
+    t.string "body"
+    t.string "title"
+    t.integer "likes_count"
+    t.integer "dislikes_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_06_01_194727) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "questions", "users"
 end
