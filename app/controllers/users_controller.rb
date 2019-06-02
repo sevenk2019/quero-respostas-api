3b7d78815aa0592c7d2fe0036aafae478e2f343c
robisnei <robisneey@hotmail.com>
2019-06-02 09:30:07 -0300

    return token in create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,10 +24,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if @user
+      token = Auth.new.encode(user: @user.id)
+      response.set_header('Authorization', "Bearer #{token}")
+      render json: { jwt: token }
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: "invalid params" }, status: 401
     end
   end
 
