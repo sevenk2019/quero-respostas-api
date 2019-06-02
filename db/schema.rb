bf98700ad9c3ca6fec2b742c4eec150ecdccfcf1
robisnei <robisneey@hotmail.com>
2019-06-02 06:16:34 -0300

    add migrate and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_071315) do
+ActiveRecord::Schema.define(version: 2019_06_02_091037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2019_06_02_071315) do
     t.string "parent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "kind"
   end
 
   create_table "tags_users", id: false, force: :cascade do |t|
