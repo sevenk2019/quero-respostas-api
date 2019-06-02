f98ed7e0fb4b3d16a8464bdb2655794e8f06ee7c
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-06-01 22:17:19 -0300

    Add current_user & logged_id to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+    def current_user
+        if http_authorization?
+            decoded_id = Auth.new.decode(token)['user']
+            @current_user ||= User.find(decoded_id)
+        end
+    end
+
+    def logged_id?
+        current_user.present?
+    end
+
+    def token
+        request
+            .env['HTTP_AUTHORIZATION']
+            .scan(/Bearer (.*)$/)
+            .flatten
+            .last
+    end
+
+    def http_authorization?
+        request
+            .env
+            .fetch('HTTP_AUTHORIZATION', '')
+            .scan(/Bearer/)
+            .flatten
+            .first
+            .present?
+    end
 end
