f02091e62167f037632fa441225af910b359c67a
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-06-02 03:18:30 -0300

    Fix production autoload

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module QueroRespostasApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
     config.api_only = true
   end
 end
